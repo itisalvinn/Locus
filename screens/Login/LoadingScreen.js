7b1d917ba94bb7d0d61c81dea3104e48ea0b2f75
Anna Do <anna.do@uwaterloo.ca>
2019-07-20 15:02:46 -0400

    House functionality

--- screens/Login/LoadingScreen.js
+++ screens/Login/LoadingScreen.js
@@ -30,7 +30,6 @@ class LoadingScreen extends Component {
               houseUuid = key;
             }
           }
-          // TODO: Get the houseInfo and then navigate to it
           return this.props.navigation.navigate(
             'DashboardScreen',
             {user, houseUuid, uid: authUser.uid}
