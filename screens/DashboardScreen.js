9653c21e50612b13479bb19b0dffce8b112145a0
Anna Do <anna.do@uwaterloo.ca>
2019-07-21 14:22:40 -0400

    House functionality (#9)
    * House functionality
    
    * todo
    
    * edge case
    
    * loading until houseInfo shows
    
    * add a demo house
    
    * comment optional for second editHouse param

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import {Button, StyleSheet, Text, View, AsyncStorage, ActivityIndicator} from 'react-native';
 import {BottomNavigation, BottomNavigationTab} from 'react-native-ui-kitten';
 import TodoList from './TodoList/TodoList';
+import House from './House/House';
 import {authDetect, base, authSignOut} from '../firebase';
-
 
 class DashboardScreen extends Component {
   constructor(props) {
@@ -11,20 +11,27 @@ class DashboardScreen extends Component {
     this.state = {
       items: {},
       itemKeys: [],
-      uid: null,
+      uid: props.navigation.state.params ? props.navigation.state.params.uid : null,
       selectedIndex: 0,
+      user: props.navigation.state.params ? props.navigation.state.params.user : null,
+      houseUuid: props.navigation.state.params ? props.navigation.state.params.houseUuid : null,
+      houseInfo: null,
     }
   }
 
   componentDidMount() {
-    AsyncStorage.getItem('uid').then(uid => {
-      this.setState({uid});
-      this.synchronizeStatesWithFirebase(uid);
-    });
+    this.synchronizeStatesWithFirebase(this.state.uid);
+    if (this.state.houseUuid) {
+      this.synchronizeHouseStatesWithFirebase(this.state.houseUuid)
+    } else {
+      // For the demo:
+      this.editHouse('demo-housing2', {name: 'Demo Housing2'});
+    }
   }
 
   componentWillUnmount() {
-    this.removeBindingFromFirebase()
+    this.removeBindingFromFirebase();
+    this.removeHouseBindingFromFirebase();
   }
 
   synchronizeStatesWithFirebase(uid) {
@@ -36,11 +43,27 @@ class DashboardScreen extends Component {
       context: this,
       state: "itemKeys"
     });
+    this.userRef = base.syncState(`users/${uid}`, {
+      context: this,
+      state: "user"
+    });
+  }
+
+  synchronizeHouseStatesWithFirebase(houseUuid) {
+    this.houseInfoRef = base.syncState(`houses/${houseUuid}`, {
+      context: this,
+      state: "houseInfo"
+    });
   }
 
   removeBindingFromFirebase() {
     base.removeBinding(this.itemsRef);
     base.removeBinding(this.itemKeysRef);
+    base.removeBinding(this.userRef);
+  }
+
+  removeHouseBindingFromFirebase() {
+    base.removeBinding(this.houseInfoRef);
   }
 
   toggleItemComplete = (key) => {
@@ -82,9 +105,7 @@ class DashboardScreen extends Component {
   deleteItem = (key) => {
     const {items} = this.state;
     let {itemKeys} = this.state;
-
     items[key] = null;
-
     itemKeys = itemKeys.filter(item => item != key);
 
     this.setState({
@@ -105,37 +126,219 @@ class DashboardScreen extends Component {
     this.setState({selectedIndex});
   }
 
+  signout = async () => {
+    await AsyncStorage.setItem('uid', null);
+    await AsyncStorage.setItem('houseUuid', null);
+    authSignOut(this.onSuccess, this.onError);
+  }
+
+  editHouse = (houseUuid, newHouseInfo /* optional */) => {
+    // 1. Remove binding for the current house
+    if (this.houseInfoRef) {
+      this.removeHouseBindingFromFirebase();
+    }
+
+    // 2. Update the current state with new houseUuid 
+    base
+      .fetch(`houses/${houseUuid}`, {
+        context: this,
+      })
+      .then(data => {
+        this.setState({
+          houseInfo: data
+        });
+
+        // 3. Synchronize with new houseUuid
+        this.synchronizeHouseStatesWithFirebase(houseUuid);
+
+        let {user, houseInfo, uid} = this.state;
+        let {members = {}} = houseInfo;
+        user = {
+          ...user,
+          houses: {
+            ...user.houses,
+            [houseUuid]: Date.now()
+          }
+        };
+        houseInfo = {
+          ...houseInfo,
+          ...newHouseInfo,
+          members: {
+            ...members,
+            [uid]: true
+          },
+        };
+        this.setState({houseInfo, user, houseUuid});
+      })
+      .catch(error => {
+        console.log("[editHouse] ", error);
+      });
+  }
+
+  leaveHouse = (houseUuid) => {
+      const oldHouseUuid = this.state.houseUuid;
+
+      if (this.houseInfoRef) {
+        this.removeHouseBindingFromFirebase();
+      }
+
+        // 2. Update the current state with new houseUuid 
+        base
+          .fetch(`houses/${houseUuid}`, {
+            context: this,
+          })
+          .then(data => {
+            this.setState({
+              houseInfo: data
+            });
+    
+            // 3. Synchronize with new houseUuid
+            this.synchronizeHouseStatesWithFirebase(houseUuid);
+    
+            let {user, uid, houseInfo} = this.state;
+            let {name = '', members = {}} = houseInfo;
+          user = {
+            ...user,
+            houses: {
+              ...user.houses,
+              [houseUuid]: null
+            }
+          };
+          houseInfo = {
+            ...houseInfo,
+            members: {
+              ...members,
+              [uid]: null
+            },
+          };
+
+          // 4. Check if members is now empty
+          const validMembersLen = Object.keys(houseInfo.members).filter(member => houseInfo.members[member] !== null).length;
+          if (!validMembersLen) {
+            // House has no member
+            houseInfo = null;
+          }
+          this.setState({houseInfo, user});
+
+            // 5. Redirect to the new houseUuid
+            const newHouseUuid = this.getNewLastHouse(oldHouseUuid);
+            if (newHouseUuid) {
+              if (this.houseInfoRef) {
+                this.removeHouseBindingFromFirebase();
+              }
+
+              base
+              .fetch(`houses/${newHouseUuid}`, {
+                context: this,
+              })
+              .then(newData => {
+                this.setState({
+                  houseInfo: newData
+                });
+
+                // 3. Synchronize with newHouseUuid
+                this.synchronizeHouseStatesWithFirebase(newHouseUuid);
+
+                let {user: newUser, houseInfo: newHouseInfo, uid} = this.state;
+                let {members: newMembers = {}} = newHouseInfo;
+                newUser = {
+                  ...newUser,
+                  houses: {
+                    ...newUser.houses,
+                    [newHouseUuid]: Date.now()
+                  }
+                };
+                newMembers[uid] = true;
+                this.setState({
+                  houseInfo: {
+                    ...newHouseInfo,
+                    members: newMembers
+                  },
+                  user: newUser,
+                  houseUuid: newHouseUuid
+                });
+              })
+              .catch(error => {
+                console.log("Couldn't find newHouseUuid")
+              });
+            }
+          })
+          .catch(error => {
+            console.log(error);
+          });
+  }
+
+  getNewLastHouse = (avoidHouseUuid) => {
+    const {user} = this.state;
+    if (!user.houses) {
+      return null;
+    }
+    const {houses} = user;
+    let latestTimestamp = 0;
+    let houseUuid = null;
+    const houseKeys = Object.keys(houses);
+    for (let i = 0; i < houseKeys.length; i++) {
+      const key = houseKeys[i];
+      if (key === avoidHouseUuid) continue;
+      if (houses[key] >= latestTimestamp) {
+        latestTimestamp = houses[key];
+        houseUuid = houseKeys[i];
+      }
+    }
+
+    return houseUuid;
+  }
+
   renderSelectedPage() {
     const {selectedIndex} = this.state;
-
-    if (selectedIndex === 1) {
-      return (
-        <TodoList
-          key='1'
-          items={this.state.items}
-          itemKeys={this.state.itemKeys}
-          deleteItem={this.deleteItem}
-          editItem={this.editItem}
-          addItem={this.addItem}
-          toggleItemComplete={this.toggleItemComplete}
-        />
-      );
-    } else {
-      return (
-        <Button
-          key='3'
-          title="Log out"
-          onPress={() => authSignOut(this.onSuccess, this.onError)}
-          style={styles.logoutBtn}
-        >
+    switch (selectedIndex) {
+      case 0:
+        return (
+          <House
+            uid={this.state.uid}
+            user={this.state.user}
+            editHouse={this.editHouse}
+            leaveHouse={this.leaveHouse}
+            houseUuid={this.state.houseUuid}
+            houseInfo={this.state.houseInfo}
+          />
+        );
+      case 1:
+        return (
+          <TodoList
+            key='1'
+            items={this.state.items}
+            itemKeys={this.state.itemKeys}
+            deleteItem={this.deleteItem}
+            editItem={this.editItem}
+            addItem={this.addItem}
+            toggleItemComplete={this.toggleItemComplete}
+          />
+        );
+      case 4:
+        return (
+          <Button
+            title="Log out"
+            onPress={async () => await this.signout()}
+            style={styles.logoutBtn}
+          >
           Log Out
-        </Button>
-      );
+          </Button>
+        )
+      default:
+        return null;
     }
   }
 
   render() {
-    if (!this.state.uid || !this.itemsRef || !this.itemKeysRef) {
+    if (
+      !this.state.uid ||
+      !this.state.user ||
+      !this.itemsRef ||
+      !this.itemKeysRef ||
+      !this.userRef ||
+      this.state.user && this.state.user.houses && !this.state.houseInfo
+      ) {
       return (
         <View style={styles.container}>
           <ActivityIndicator size={"large"}/>
@@ -149,7 +352,6 @@ class DashboardScreen extends Component {
         </View>
 
         <View
-          key='2'
           style={styles.bottomNav}>
           <BottomNavigation
             indicatorStyle={styles.indicator}
@@ -159,6 +361,8 @@ class DashboardScreen extends Component {
             <BottomNavigationTab title='To Do List'/>
             <BottomNavigationTab title='Grocery List'/>
             <BottomNavigationTab title='Settings'/>
+            {/* Below is temporary */}
+            <BottomNavigationTab title='Logout' /> 
           </BottomNavigation>
         </View>
       </View>
